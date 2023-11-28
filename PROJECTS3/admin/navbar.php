48580023353c301e094761923888317e20d13199
Alvinavina05 <124978616+Alvinavina05@users.noreply.github.com>
2023-11-28 21:02:16 +0700

    penambahan coding logout
    penambahan logout

--- PROJECTS3/admin/navbar.php
+++ PROJECTS3/admin/navbar.php
@@ -5,7 +5,31 @@ if (!isset($_SESSION['nama_lengkap'])) {
     exit(); // Terminate script execution after the redirect
 }
 
-?>
+?><script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+<script >
+
+function contohAksi() {
+    Swal.fire({
+        // title: 'Berhasil!',
+        title: 'Apakah Kamu Ingin Logout?',
+        icon: 'question',
+        color: '#fff',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Logout',
+        background: '#081226'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Redirect ke logout.php atau tempat logout sesuai kebutuhan
+            window.location.href = '../KONEKSI/logout.php';
+        }
+    });
+}
+
+</script>
 
 <nav class="navbar navbar-expand bg-secondary navbar-dark sticky-top px-4 py-0">
                 <a href="#" class="navbar-brand d-flex d-lg-none me-4">
@@ -18,7 +42,7 @@ if (!isset($_SESSION['nama_lengkap'])) {
                     <input class="form-control  text-center bg-dark border-0" valu="" id="jamInput" readonly type="search">
                 </form>
                 <div class="navbar-nav align-items-center ms-auto">
-                    <div class="nav-item dropdown">
+                    <!-- <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                             <i class="fa fa-envelope me-lg-2"></i>
                             <span class="d-none d-lg-inline-flex">Message</span>
@@ -56,8 +80,8 @@ if (!isset($_SESSION['nama_lengkap'])) {
                             <hr class="dropdown-divider">
                             <a href="#" class="dropdown-item text-center">See all message</a>
                         </div>
-                    </div>
-                    <div class="nav-item dropdown">
+                    </div> -->
+                    <!-- <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                             <i class="fa fa-bell me-lg-2"></i>
                             <span class="d-none d-lg-inline-flex">Notificatin</span>
@@ -80,18 +104,20 @@ if (!isset($_SESSION['nama_lengkap'])) {
                             <hr class="dropdown-divider">
                             <a href="#" class="dropdown-item text-center">See all notifications</a>
                         </div>
-                    </div>
+                    </div> -->
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                            <img class="rounded-circle me-lg-2" src="../ASET/img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                            <img class="rounded-circle me-lg-2" src="../ASET/img/user.png" alt="" style="width: 40px; height: 40px;">
                             <span class="d-none d-lg-inline-flex">Hello, 
                                 <?php echo $_SESSION['nama_lengkap']; ?>
                             </span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end bg-secondary border-0 rounded-0 rounded-bottom m-0">
-                            <a href="#" class="dropdown-item">My Profile</a>
-                            <a href="#" class="dropdown-item">Settings</a>
-                            <a href="../KONEKSI/logout.php" class="dropdown-item">Log Out</a>
+                            <!-- <a href="#" class="dropdown-item">My Profile</a>
+                            <a href="#" class="dropdown-item">Settings</a> -->
+                            <button onclick="contohAksi()" class="dropdown-item">Log Out</button>
+                            <!-- <a href="../KONEKSI/logout.php" class="dropdown-item">Log Out</a> -->
+                           
                         </div>
                     </div>
                 </div>
