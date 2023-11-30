ce6cf26a07e68ae1532bf215d251e7288112601e
Alvinavina05 <124978616+Alvinavina05@users.noreply.github.com>
2023-11-30 20:46:45 +0700

    koneksi fix

--- PROJECTS3/koneksi/koneksi.php
+++ PROJECTS3/koneksi/koneksi.php
@@ -1,5 +1,5 @@
 <?php 
-$koneksi = mysqli_connect("localhost", "root", "", "vosis");
+$koneksi = mysqli_connect("localhost", "sivosism_db", "26Desember2003.", "sivosism_db");
 
 if (!$koneksi) {
     die("Koneksi database gagal: " . mysqli_connect_error());
@@ -75,7 +75,7 @@ require '../phpqrcode/qrlib.php';
 
 
 		$query = "INSERT INTO `tb_akun` (`nis_nip`, `nama_lengkap`, `jenis_kelamin`, `kelas`, `id_posisi`, `status`, `pass`, `qr`) 
-		VALUES ('$nisnip', '$namalengkap', '$jeniskelamin', '$kelas', '$idposisi', '$status', '$pass', 'http://localhost/PROJECTS3/aset/gambarqr/$namalengkap $kelas.png');";
+		VALUES ('$nisnip', '$namalengkap', '$jeniskelamin', '$kelas', '$idposisi', '$status', '$pass', 'https://sivosis.my.id/aset/gambarqr/$namalengkap $kelas.png');";
 		
 		mysqli_query($koneksi, $query);
 	
@@ -87,17 +87,21 @@ require '../phpqrcode/qrlib.php';
 
 //	// C-R-U-D / posisi  //  //
 function tambahposisi($data){
-	global $koneksi;
+    global $koneksi;
+    
+	$jenis_posisi = htmlspecialchars($data["jenis_posisi"]);
+	$id_posisi = htmlspecialchars($data["id_posisi"]);
 
-	$nama = htmlspecialchars($data["id_posisi"]);
+    // Query untuk menambahkan data ke dalam tabel tb_posisi
+    $query = "INSERT INTO tb_posisi (`id_posisi`, `jenis_posisi`)
+            VALUES
+            ('$id_posisi', '$jenis_posisi')";
 
-		$query = "INSERT INTO tb_posisi ( `jenis_posisi`)
-				VALUES
-				('$nama')";
+    // Melakukan eksekusi query menggunakan koneksi database
+    mysqli_query($koneksi, $query);
 
-	mysqli_query($koneksi, $query);
-
-	return mysqli_affected_rows($koneksi);
+    // Mengembalikan jumlah baris yang terpengaruh oleh operasi terakhir
+    return mysqli_affected_rows($koneksi);
 }
 
 function hapusposisi($id){
@@ -246,28 +250,79 @@ function hapuskandidat($id_kandidat){
 }
 
 
-	function ubahkandidat($data){
+	// function ubahkandidat($data){
+	// 	global $koneksi;
+	
+	// 	$id_kandidat = $data["id_kandidat"];
+	// 	$visi = htmlspecialchars($data["visi"]);
+	// 	$misi = htmlspecialchars($data["misi"]);
+	// 	$id_ketua = htmlspecialchars($data["id_ketua"]);
+	// 	$id_wakil = htmlspecialchars($data["id_wakil"]);
+
+	
+	// 	$query = "UPDATE `tb_kandidat` SET 
+	// 	`visi` = '$visi', 
+	// 	`misi` = '$misi',
+	// 	`id_ketua` = '$id_ketua', 
+	// 	`id_wakil` = '$id_wakil'  
+	// 	WHERE `id_kandidat` = '$id_kandidat'";
+	
+	// 	mysqli_query($koneksi, $query);
+	
+	// 	return mysqli_affected_rows($koneksi);
+	// }
+
+	function ubahkandidat($data) {
 		global $koneksi;
 	
-		$id_kandidat = $data["id_kandidat"];
+		$id_kandidat = htmlspecialchars($data["id_kandidat"]);
 		$visi = htmlspecialchars($data["visi"]);
 		$misi = htmlspecialchars($data["misi"]);
 		$id_ketua = htmlspecialchars($data["id_ketua"]);
 		$id_wakil = htmlspecialchars($data["id_wakil"]);
-
 	
-		$query = "UPDATE `tb_kandidat` SET 
-		`id_kandidat` = '$id_kandidat', 
-		`visi` = '$visi', 
-		`misi` = '$misi',
-		`id_ketua` = '$id_ketua', 
-		`id_wakil` = '$id_wakil',  
-		WHERE `tb_kandidat`.`id_kandidat` = '$id_kandidat';";
+		// Proses upload gambar
+		$upload_dir = "../aset/gambarkandidat/";
+		$nama_gambar = '';
+	
+		if (isset($_FILES["gambar"]) && $_FILES["gambar"]["error"] === UPLOAD_ERR_OK) {
+			$img_temp = $_FILES["gambar"]["tmp_name"];
+			$ekstensi_gambar = pathinfo($_FILES["gambar"]["name"], PATHINFO_EXTENSION);
+			$nama_gambar = uniqid() . '.' . $ekstensi_gambar;
+	
+			if (move_uploaded_file($img_temp, $upload_dir . $nama_gambar)) {
+				$nama_gambar = 'https://sivosis.my.id/aset/gambarkandidat/' . $nama_gambar;
+			} else {
+				echo json_encode(array('success' => false, 'message' => 'Gagal mengupload gambar.'));
+				exit();
+			}
+		}
+	
+		// Cek apakah gambar baru diupload
+		if ($nama_gambar != '') {
+			$query = "UPDATE `tb_kandidat` SET 
+				`visi` = '$visi', 
+			 	`misi` = '$misi',
+			 	`id_ketua` = '$id_ketua', 
+				`id_wakil` = '$id_wakil' ,
+				`gambar` = '$nama_gambar'  
+				WHERE tb_kandidat.id_kandidat = '$id_kandidat'";
+		} else {
+			// Jika gambar tidak diubah, tetap simpan data gambar lama
+			$query = "UPDATE `tb_kandidat` SET 
+			`visi` = '$visi', 
+			 `misi` = '$misi',
+			 `id_ketua` = '$id_ketua', 
+			`id_wakil` = '$id_wakil', 
+			`gambar` = '$nama_gambar'  
+			WHERE tb_kandidat.id_kandidat = '$id_kandidat'";
+		}
 	
 		mysqli_query($koneksi, $query);
 	
 		return mysqli_affected_rows($koneksi);
 	}
+	
 
 
 
@@ -279,18 +334,38 @@ function hapuskandidat($id_kandidat){
 		$misi = htmlspecialchars($data["misi"]);
 		$id_ketua = htmlspecialchars($data["id_ketua"]);
 		$id_wakil = htmlspecialchars($data["id_wakil"]);
-		$gambar = htmlspecialchars($data["gambar"]);
 
+		    // Proses upload gambar
+			$upload_dir = "../aset/gambarkandidat/"; // Ganti dengan direktori tempat Anda ingin menyimpan gambar
+			$nama_gambar = '';
+		
+			if (isset($_FILES["gambar"]) && $_FILES["gambar"]["error"] === UPLOAD_ERR_OK) {
+				$img_temp = $_FILES["gambar"]["tmp_name"];
+				$ekstensi_gambar = pathinfo($_FILES["gambar"]["name"], PATHINFO_EXTENSION);
+				$nama_gambar = uniqid() . '.' . $ekstensi_gambar;
+		
+				if (move_uploaded_file($img_temp, $upload_dir . $nama_gambar)) {
+					$nama_gambar = 'https://sivosis.my.id/aset/gambarkandidat/' . $nama_gambar;
+				} else {
+					echo json_encode(array('success' => false, 'message' => 'Gagal mengupload gambar.'));
+					exit();
+				}
+			}
 
-		$query = "INSERT INTO `tb_kandidat` (`id_kandidat`, `visi`, `misi`, `id_ketua`, `id_wakil`, `gambar`) 
-		VALUES ('$id_kandidat', '$visi', '$misi', '$id_ketua', '$id_wakil', '$gambar');";
+		$query = "INSERT INTO tb_kandidat (id_kandidat, visi, misi, id_ketua, id_wakil , gambar) 
+		VALUES ('$id_kandidat', '$visi', '$misi', '$id_ketua', '$id_wakil', '$nama_gambar');";
 		
 		mysqli_query($koneksi, $query);
 	
 		return mysqli_affected_rows($koneksi);
 	}
+// CRUD VOTING
 
-	// CRUD VOTING
+	function hapusvoting($id){
+		global $koneksi;
+		mysqli_query($koneksi, "DELETE FROM tb_voting WHERE `tb_voting`.`nis_nip` = '$id'");
+		return mysqli_affected_rows($koneksi);
+	}
 	function tambahvoting($data){
 		global $koneksi;
 	
